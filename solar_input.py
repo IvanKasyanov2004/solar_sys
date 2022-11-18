cb32761962e5475d4cba4f0f049610166cfb9be6
physicistmaksim <113688831+physicistmaksim@users.noreply.github.com>
2022-11-18 15:04:31 +0300

    Commit test 2

--- solar_input.py
+++ solar_input.py
@@ -7,7 +7,7 @@ from solar_vis import DrawableObject
 def read_space_objects_data_from_file(input_filename):
     """Cчитывает данные о космических объектах из файла, создаёт сами объекты
     и вызывает создание их графических образов
-        ОБАМА
+        ОБЭМЭ
     Параметры:
 
     **input_filename** — имя входного файла
